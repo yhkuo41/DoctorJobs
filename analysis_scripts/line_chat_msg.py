d9679e47d2d99a24ba76e2466411aa5cd520373c
Y.H. Kuo <yhkuo41@gmail.com>
2023-03-14 20:37:08 +0800

    Initial implementation of the message filter

--- analysis_scripts/line_chat_msg.py
+++ analysis_scripts/line_chat_msg.py
@@ -1,26 +1,32 @@
 def csv_header() -> list[str]:
-    return ["utc_ts", "user", "content", "is_recruitment_man", "is_recruitment_algo", "region_tags", "department_tags"]
+    return ["utc_ts", "user", "content", "is_recruitment", "city_tags", "dept_tags"]
+
+
+def set2str(s: set[str]):
+    return "/".join(s)
+
+
+def str2set(s: str):
+    return set(s.split("/"))
 
 
 class LineChatMsg:
     def __init__(self,
-                 utc_ts,
+                 utc_ts: int,
                  user: str,
                  content: str,
-                 region_tags=None,
-                 department_tags=None,
-                 is_recruitment_msg_algo=False,
-                 is_recruitment_msg_man=False):
-        if not region_tags or region_tags == "[]":
-            region_tags = []
-        if not department_tags or department_tags == "[]":
-            department_tags = []
-        self.is_recruitment_algo = bool(is_recruitment_msg_algo)
-        self.is_recruitment_man = bool(is_recruitment_msg_man)
-        self.department_tags = department_tags
-        self.region_tags = region_tags
+                 city_tags: set[str] = None,
+                 dept_tags: set[str] = None,
+                 is_recruitment: bool = False):
+        if not city_tags:
+            city_tags = set()
+        if not dept_tags:
+            dept_tags = set()
+        self.is_recruitment = is_recruitment
+        self.dept_tags = dept_tags
+        self.city_tags = city_tags
         self.content = content
-        self.utc_ts = int(utc_ts)
+        self.utc_ts = utc_ts
         self.user = user
 
     def to_csv_row(self) -> list:
@@ -28,8 +34,7 @@ class LineChatMsg:
             self.utc_ts,
             self.user,
             self.content,
-            str(self.is_recruitment_man)[0],
-            str(self.is_recruitment_algo)[0],
-            self.region_tags,
-            self.department_tags
+            str(self.is_recruitment)[0],
+            set2str(self.city_tags),
+            set2str(self.dept_tags)
         ]
--- analysis_scripts/msg_filter.py
+++ analysis_scripts/msg_filter.py
@@ -1,7 +1,7 @@
 from analysis_scripts.line_chat_msg import LineChatMsg
 
 
-class JobMsgFilter:
+class KeywordFilter:
     def __init__(self, keywords=None):
         if keywords is None:
             keywords = {}
@@ -22,4 +22,42 @@ class JobMsgFilter:
         for k in self.keywords:
             if k in msg:
                 return True
+        return False
+
+
+class StrLenFilter:
+    def __init__(self, min_len: int):
+        self.min_len = min_len
+
+    def apply(self, msg) -> bool:
+        """
+        return True if the msg is a recruitment message
+        """
+        if isinstance(msg, str):
+            return len(msg) >= self.min_len
+        elif isinstance(msg, LineChatMsg):
+            return len(msg.content) >= self.min_len
+
+        raise NotImplementedError(f"not support msg type: {type(msg)}")
+
+
+class DeptFilter:
+    @staticmethod
+    def apply(msg: LineChatMsg) -> bool:
+        """
+        return True if the msg is a recruitment message
+        """
+        if msg.dept_tags:
+            return True
+        return False
+
+
+class CityFilter:
+    @staticmethod
+    def apply(msg: LineChatMsg) -> bool:
+        """
+        return True if the msg is a recruitment message
+        """
+        if msg.city_tags:
+            return True
         return False
